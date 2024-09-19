58c1b5b3b04f22201add58822d792d7a1b17504c
Nathan Zweifel <33692087+zweifelna@users.noreply.github.com>
2024-09-19 21:02:35 +0200

    change url with custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/personnal-website/',  // Remplace 'ton-repo' par le nom de ton dépôt GitHub
-  site: 'https://zweifelna.github.io/personnal-website/',  // URL complète vers ton GitHub Pages
+  site: 'https://nathanzweifel.ch',  // URL complète vers ton GitHub Pages
 });
