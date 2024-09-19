8682ae939296dcffb4f1de2da08d033f4cfc3e3f
Nathan Zweifel <33692087+zweifelna@users.noreply.github.com>
2024-09-19 14:50:46 +0200

    still changing some messed up url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/ton-repo/',  // Remplace 'ton-repo' par le nom de ton dépôt GitHub
+  base: '/https://github.com/zweifelna/personnal-website/',  // Remplace 'ton-repo' par le nom de ton dépôt GitHub
   site: 'https://zweifelna.github.io/personnal-website/',  // URL complète vers ton GitHub Pages
 });
