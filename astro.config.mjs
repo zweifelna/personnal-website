9d9bf6e7af5d583411b8074490c6912a547948fc
Nathan Zweifel <33692087+zweifelna@users.noreply.github.com>
2024-09-19 14:46:10 +0200

    Fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   base: '/ton-repo/',  // Remplace 'ton-repo' par le nom de ton dépôt GitHub
-  site: 'https://ton-nom-utilisateur.github.io/ton-repo/',  // URL complète vers ton GitHub Pages
+  site: 'https://zweifelna.github.io/personnal-website/',  // URL complète vers ton GitHub Pages
 });
